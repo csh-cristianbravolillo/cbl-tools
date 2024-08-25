7547de416020d566740ac506cd170ca708720d3e
Cristian Bravo Lillo <cristian.bravo@gmail.com>
2024-08-25 16:57:20 -0400

    Last step in separating cbl_tools from cylon

--- src/cbl_tools/config.py
+++ src/cbl_tools/config.py
@@ -16,47 +16,49 @@ import atexit
 import errno
 import configparser
 from configparser import ExtendedInterpolation
-
+from cbl_tools.exceptions import *
 
 class config:
 
     path = None
     values = None
 
-    def __init__(self, initpath:str = '~/.config', filename:str = 'config.ini', create_folder:bool = True, default_section:str = 'default') -> None:
+    def __init__(self, initpath:str = '~/.config/config.ini', create_folder:bool = True, default_section:str = 'default') -> None:
         """It creates a config file.
 
-        To create the file, we specify its path (by default, '~/.config'), and its filename (by default, 'config.ini'). The last folder within the path may not exist,
+        To create the file, we specify its path (by default, '~/.config/config.ini'). The last folder within the path may not exist,
         in which case it will be created. Neither initpath nor filename could be empty (an OSError will be raised). We can also specify whether an inexistent folder
         should be created with create_folder (by default, True).
 
-        If thispath or filename are empty it raises an error. If the last portion of thispath doesn't exist and create_folder is False, it raises a FileNotFoundError
+        If thispath is empty it raises an error. If the last portion of thispath doesn't exist and create_folder is False, it raises a FileNotFoundError
         since we're being asked to use a folder that doesn't exist, and we're being told not to create it.
 
-        Finally, if a file exists in path/folder/filename, it is read and put into self.values. If it doesn't exist, it will be created in path/folder/filename when the
-        program is terminated.
+        Finally, if a file exists in initpath, it's read and put into self.values. If it doesn't exist, it will be created when the
+        program is terminated, or when flush() is invoked.
         """
 
-        initpath = os.path.normpath(os.path.expanduser(initpath))
+        if not initpath:
+            raise EmptyValueError("initpath must not be empty")
 
-        if initpath == ".":
-            raise OSError(errno.EINVAL, "path cannot be empty")
+        folder,filename = os.path.split(os.path.normpath(os.path.expanduser(initpath)))
 
-        if not filename:
-            raise OSError(errno.EINVAL, "filename cannot be empty")
-
-        if not os.path.exists(initpath) and not create_folder:
-            raise FileNotFoundError(errno.ENOENT, "path does not exist and create_folder is False", initpath)
+        if not os.path.exists(folder) and not create_folder:
+            raise FileNotFoundError(errno.ENOENT, "path does not exist and create_folder is False", folder)
 
         try:
-            if not os.path.exists(initpath) and create_folder:
-                os.mkdir(initpath, 0o700)
-        except FileNotFoundError:
-            raise FileNotFoundError(errno.ENOENT, "one or more of the parent folders in thispath don't exist, and won't be created", initpath)
-        except PermissionError:
-            raise PermissionError(errno.EACCES, "cannot create such folder: permission denied", initpath)
+            if not os.path.exists(folder) and create_folder:
+                os.mkdir(folder, 0o700)
 
-        self.path = os.path.normpath(os.path.join(initpath, filename))
+        except FileNotFoundError:
+            raise FileNotFoundError(errno.ENOENT, "one or more of the parent folders in thispath don't exist, and won't be created", folder)
+
+        except PermissionError:
+            raise PermissionError(errno.EACCES, "cannot create folder: permission denied", folder)
+
+        self.path = os.path.join(folder, filename)
+
+        if os.path.exists(self.path) and not os.path.isfile(self.path):
+            raise NotAFileError("last part of initpath already exists, and it's not a file")
 
         # If path goes out of normalized init_path, the user may be abusing this class, so we ban it
         if os.path.commonpath([self.path, initpath]) != initpath:
@@ -69,27 +71,35 @@ class config:
         if os.path.exists(self.path):
             self.values.read(self.path)
 
-        self.register(self.__writedown)
+        self.register(self.flush)
+
+
+    def __str__(self) -> str:
+        return f"<cbl_tools.config object; path={self.path}>"
 
 
     def register(self, func:callable) -> None:
         atexit.register(func)
 
 
-    def __writedown(self) -> None:
+    def flush(self) -> None:
         with open(self.path, "w") as thisfile:
             self.values.write(thisfile)
 
 
     def set(self, section:str, var:str, val:str) -> None:
-        if not var or not val:
-            raise ValueError("set() was called without a var or a val")
+        if not section or section=='':
+            section = self.values.default_section
+
+        if not var:
+            raise MissingValueError("missing var")
+
         self.values.set(section, var, val)
 
 
     def get(self, section:str, var:str = '') -> str:
         if not section:
-            raise ValueError("get() was called without a section or a var")
+            section = self.values.default_section
 
         if var:
             return self.values.get(section, var)
@@ -101,9 +111,16 @@ class config:
         return self.values.sections()
 
 
+    def has_section(self, section:str) -> bool:
+        if not section:
+            section = self.values.default_section
+
+        return section in self.sections()
+
+
     def section(self, section:str) -> dict:
         if not section:
-            raise ValueError("items() was called without a section")
+            section = self.values.default_section
 
         lst = {}
         for pair in self.values.items(section):
@@ -112,7 +129,15 @@ class config:
         return lst
 
 
+    def add_section(self, section:str) -> None:
+        if not section:
+            section = self.values.default_section
+        if self.has_section(section):
+            raise ValueExistsError(f"section {section} already exists")
+        self.values.add_section(section)
+
+
     def keys(self, section:str) -> list:
         if not section:
-            raise ValueError("keys() was called without a section")
+            section = self.values.default_section
         return self.values.options(section)
--- tests/config_tests.py
+++ tests/config_tests.py
@@ -8,28 +8,23 @@ class ConfigTests(unittest.TestCase):
     x = None
 
     def setUp(self) -> None:
-        self.x = config.config(create_tempdir(True), 'zort.ini')
+        self.x = config.config(initpath=create_tempdir(True) + '/zort.ini', default_section='base')
         return super().setUp()
 
+    def _create_values(self):
+        self.x.add_section('blabla')
+        self.x.set('blabla', 'a', '1')
+        self.x.set('blabla', 'b', 'be')
+        self.x.set('blabla', 'c', '3.141592653')
+        self.x.set('blabla', 'switch', 'on')
+        self.x.add_section('naka')
+        self.x.set('naka', 'valor1', 'a${blabla:a}c${blabla:c}zzz${blabla:b}')
+        self.x.set(None, 'tres', 'cuatro')
 
-    def test_trivial_exceptions(self):
-
-        # Trivial exceptions
-        try:
-            config.config(initpath = "")
-        except OSError as e:
-            self.assertEqual(e.errno, errno.EINVAL, "Empty path should raise an OSError")
-
-        try:
-            config.config(filename = "")
-        except OSError as e:
-            self.assertEqual(e.errno, errno.EINVAL, "Empty filename should raise an OSError")
-
-
-    def test_not_trivial_exceptions(self):
+    def test_exceptions(self):
         # Opening an inexistent folder should raise an OSError:1
         try:
-            config.config(initpath = os.path.join(tempfile.gettempdir(), "inexistent_folder_029837492837498"), create_folder=False)
+            config.config(initpath = os.path.join(tempfile.gettempdir(), "inexistent_folder_029837492837498/file.txt"), create_folder=False)
         except FileNotFoundError as e:
             self.assertEqual(e.errno, errno.ENOENT, "An inexistent path with false create_folder should raise a FileNotFoundError")
 
@@ -39,59 +34,55 @@ class ConfigTests(unittest.TestCase):
             self.assertEqual(e.errno, errno.ENOENT, "More than two inexistent folders should raise a FileNotFoundError")
 
         try:
-            config.config(initpath = '/root/my_own_folder')
+            config.config(initpath = '/root/my_own_folder/file.txt')
         except PermissionError as e:
             self.assertEqual(e.errno, errno.EACCES, "Trying to create a folder within /root should raise a PermissionError!")
 
-
     def test_create_normal_config(self):
-
         # If asked to open an inexistent folder within a valid path, I should be able to create it.
         path = os.path.dirname(self.x.path)
         self.assertTrue(os.path.exists(path), f"Config should be able to create folder {path}")
         self.assertTrue(os.path.isdir(path), f"{path} should be a folder (it's not)")
 
         # Create a few values and then retrieve them
-        self.assertFalse(self.x.values.has_section('blabla'), "An inexistent section ('blabla') exists")
-        self.x.values.add_section('blabla')
-        self.assertTrue(self.x.values.has_section('blabla'), "Cannot create section 'blabla'")
-        self.x.values.set('blabla', 'a', '1')
-        self.x.values.set('blabla', 'b', 'be')
-        self.x.values.set('blabla', 'c', '3.141592653')
-        self.x.values.set('blabla', 'switch', 'on')
-
-        self.assertEqual(self.x.values.get('blabla', 'a'), '1', 'get(blabla:a) should be equal to \'1\'; it\'s not')
-        self.assertEqual(self.x.values.getint('blabla', 'a'), 1, 'getint(blabla:a) should be equal to 1; it\'s not')
-        self.assertEqual(self.x.values.get('blabla', 'b'), 'be', 'get(blabla:b) should be equal to \'be\'; it\'s not')
-        self.assertEqual(self.x.values.getfloat('blabla', 'c'), 3.141592653, 'getint(blabla:c) should be equal to pi; it\'s not')
-        self.assertTrue(self.x.values.getboolean('blabla', 'switch'), 'getint(blabla:switch) should be True; it\'s not')
-
+        self.assertFalse(self.x.has_section('blabla'), "An inexistent section ('blabla') exists")
+        self._create_values()
+        self.assertTrue(self.x.has_section('blabla'), "Cannot create section 'blabla'")
+        self.assertEqual(self.x.get('blabla', 'a'), '1', 'get(blabla:a) should be equal to \'1\'; it\'s not')
+        self.assertEqual(int(self.x.get('blabla', 'a')), 1, 'getint(blabla:a) should be equal to 1; it\'s not')
+        self.assertEqual(self.x.get('blabla', 'b'), 'be', 'get(blabla:b) should be equal to \'be\'; it\'s not')
+        self.assertEqual(float(self.x.get('blabla', 'c')), 3.141592653, 'getint(blabla:c) should be equal to pi; it\'s not')
+        self.assertTrue(bool(self.x.get('blabla', 'switch')), 'getbool(blabla:switch) should be True; it\'s not')
 
     def test_interpolation(self):
-
-        # Test interpolation
-        self.x.values.add_section('blabla')
-        self.assertTrue(self.x.values.has_section('blabla'), "Cannot create section 'blabla'")
-
-        self.x.values.set('blabla', 'a', '1')
-        self.x.values.set('blabla', 'b', 'be')
-        self.x.values.set('blabla', 'c', '3.141592653')
-        self.x.values.set('blabla', 'switch', 'on')
-
-        self.x.values.add_section('naka')
-        self.x.values.set('naka', 'valor1', 'a${blabla:a}c${blabla:c}zzz${blabla:b}')
-
-        self.assertTrue(self.x.values.has_section('naka'), "Cannot create section 'naka'")
-        self.assertEqual(self.x.values.get('naka', 'valor1'), 'a1c3.141592653zzzbe')
-        self.assertEqual(self.x.values.getboolean('blabla', 'switch'), True, "")
-
+        self._create_values()
+        self.assertTrue(self.x.has_section('naka'), "Cannot create section 'naka'")
+        self.assertEqual(self.x.get('naka', 'valor1'), 'a1c3.141592653zzzbe')
 
     def test_abusive_conf(self):
         try:
-            self.x = config.config(os.getenv("HOME"), "../another_user")
+            self.x = config.config(os.getenv("HOME") + "/../another_user")
         except ValueError:
             pass
 
+    def test_store_and_retrieve(self):
+        self._create_values()
+        self.x.flush()
+
+        y = config.config(initpath=self.x.path, create_folder=False)
+        self.assertTrue(y.has_section('blabla'), "Cannot create section 'blabla'")
+        self.assertEqual(y.get('blabla', 'a'), '1', 'get(blabla:a) should be equal to \'1\'; it\'s not')
+        self.assertEqual(y.get('blabla', 'b'), 'be', 'get(blabla:b) should be equal to \'be\'; it\'s not')
+        self.assertEqual(float(y.get('blabla', 'c')), 3.141592653, 'getint(blabla:c) should be equal to pi; it\'s not')
+        self.assertTrue(bool(y.get('blabla', 'switch')), 'getbool(blabla:switch) should be True; it\'s not')
+
+    def test_sections(self):
+        self._create_values()
+        self.assertListEqual(self.x.sections(), ['blabla', 'naka'], "sections() should return all sections, but it doesn't")
+
+    def test_keys(self):
+        self._create_values()
+        self.assertListEqual(self.x.keys('blabla'), ['a', 'b', 'c', 'switch', 'tres'])
 
 if __name__ == '__main__':
     unittest.main(verbosity=2)
